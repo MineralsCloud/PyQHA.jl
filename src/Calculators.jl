74abb59478814bc20a3ee7beadcda1974c846236
singularitti <singularitti@outlook.com>
2021-01-14 03:26:53 -0500

    Fix src/Calculators.jl

--- src/Calculators.jl
+++ src/Calculators.jl
@@ -2,7 +2,7 @@ module Calculators
 
 using PyCall: PyObject
 
-using PyQHA: qha, @pyinterface
+using PyQHA: qha_calculator, @pyinterface
 
 export SingleCalculator, SamePhDOSCalculator, DifferentPhDOSCalculator
 
@@ -10,12 +10,17 @@ abstract type Calculator end
 mutable struct SingleCalculator <: Calculator
     o::PyObject
 end
+SingleCalculator(config::AbstractDict) = SingleCalculator(qha_calculator.Calculator(config))
 mutable struct SamePhDOSCalculator <: Calculator
     o::PyObject
 end
+SamePhDOSCalculator(config::AbstractDict) =
+    SamePhDOSCalculator(qha_calculator.SamePhDOSCalculator(config))
 mutable struct DifferentPhDOSCalculator <: Calculator
     o::PyObject
 end
+DifferentPhDOSCalculator(config::AbstractDict) =
+    DifferentPhDOSCalculator(qha_calculator.DifferentPhDOSCalculator(config))
 
 @pyinterface Calculator
 
