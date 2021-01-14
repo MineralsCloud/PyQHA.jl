07f397913e732baa64c1181b74eee66e0f7d0eef
singularitti <singularitti@outlook.com>
2021-01-14 04:52:03 -0500

    Use `Config` in src/Calculators.jl

--- src/Calculators.jl
+++ src/Calculators.jl
@@ -3,6 +3,7 @@ module Calculators
 using PyCall: PyObject
 
 using PyQHA: qha_calculator, @pyinterface
+using PyQHA.Settings: Config
 
 export SingleCalculator, SamePhDOSCalculator, DifferentPhDOSCalculator
 
@@ -10,16 +11,16 @@ abstract type Calculator end
 mutable struct SingleCalculator <: Calculator
     o::PyObject
 end
-SingleCalculator(config::AbstractDict) = SingleCalculator(qha_calculator.Calculator(config))
+SingleCalculator(config::Config) = SingleCalculator(qha_calculator.Calculator(config))
 mutable struct SamePhDOSCalculator <: Calculator
     o::PyObject
 end
-SamePhDOSCalculator(config::AbstractDict) =
+SamePhDOSCalculator(config::Config) =
     SamePhDOSCalculator(qha_calculator.SamePhDOSCalculator(config))
 mutable struct DifferentPhDOSCalculator <: Calculator
     o::PyObject
 end
-DifferentPhDOSCalculator(config::AbstractDict) =
+DifferentPhDOSCalculator(config::Config) =
     DifferentPhDOSCalculator(qha_calculator.DifferentPhDOSCalculator(config))
 
 @pyinterface Calculator
