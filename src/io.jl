d26aeee2b887a5eabad692d11dd05f83de3b0c9d
singularitti <singularitti@outlook.com>
2021-01-14 04:52:17 -0500

    Update src/io.jl

--- src/io.jl
+++ src/io.jl
@@ -5,3 +5,7 @@ end
 function save_x_tv(df, t, desired_pressures_gpa, p_sample_gpa, outfile_name)
     qha_basicio_out.save_x_tv(df, t, desired_pressures_gpa, p_sample_gpa, outfile_name)
 end
+
+save_to_output(fn_output, text) = qha_basicio_out.save_to_output(fn_output, text)
+
+make_starting_string() = qha_basicio_out.make_starting_string()
