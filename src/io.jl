de2d927c0315a899ebe6ed59f2be97b603943713
singularitti <singularitti@outlook.com>
2021-01-14 03:27:05 -0500

    Fix src/io.jl

--- src/io.jl
+++ src/io.jl
@@ -1,7 +1,7 @@
 function save_x_tp(df, t, desired_pressures_gpa, p_sample_gpa, outfile_name)
-    qha.basic_io.out.save_x_tp(df, t, desired_pressures_gpa, p_sample_gpa, outfile_name)
+    qha_basicio_out.save_x_tp(df, t, desired_pressures_gpa, p_sample_gpa, outfile_name)
 end
 
 function save_x_tv(df, t, desired_pressures_gpa, p_sample_gpa, outfile_name)
-    qha.basic_io.out.save_x_tv(df, t, desired_pressures_gpa, p_sample_gpa, outfile_name)
+    qha_basicio_out.save_x_tv(df, t, desired_pressures_gpa, p_sample_gpa, outfile_name)
 end
