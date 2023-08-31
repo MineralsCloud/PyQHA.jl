b29960c5ef2235ea51b58ff5efbbbdea9d961ac4
singularitti <singularitti@outlook.com>
2023-08-30 22:01:05 -0400

    Rewrite `FromQEOutput` to `QEConverter`, deprecate `converter`

--- src/convert.jl
+++ src/convert.jl
@@ -1,14 +1,15 @@
-export converter
+export QEConverter
 
-@pymutable FromQEOutput
-FromQEOutput(inp_file_list, inp_static, inp_q_points) =
-    FromQEOutput(qha_input_maker.FromQEOutput(inp_file_list, inp_static, inp_q_points))
+abstract type Converter end
+@pymutable QEConverter Converter
+QEConverter(inp_file_list, inp_static, inp_q_points) =
+    QEConverter(qha_input_maker.FromQEOutput(inp_file_list, inp_static, inp_q_points))
 
-function converter(inp_file_list, inp_static, inp_q_points)
-    conv = FromQEOutput(inp_file_list, inp_static, inp_q_points)
+function (conv::QEConverter)
     conv.read_file_list()
     conv.read_static()
     conv.read_q_points()
     conv.read_frequency_files()
-    return conv.write_to_file()
+    conv.write_to_file()
+    return nothing
 end
